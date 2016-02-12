b0a7d51669679d204bc613f8a6c0b028cacb8c56
CuteITGuy <grisetroseciel@gmail.com>
2016-02-12 09:17:12 +0700

    Breaking changes: change all static methods to instance methods, add RegistryKey, StartupType properties

--- CB.System.ServiceProcess/Service.cs
+++ CB.System.ServiceProcess/Service.cs
@@ -1,71 +1,118 @@
 ﻿using System;
 using System.ServiceProcess;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 
 namespace CB.System.ServiceProcess
 {
-    public class Service
+    public class Service: IDisposable
     {
-        #region Methods
-        public static bool Continue(string serviceName, TimeSpan timeout)
-            => HandleService(serviceName, service => { service.Continue(); }, ServiceControllerStatus.Running, timeout);
+        #region Fields
+        private const string REGISTRY_SERVICE_PATH = @"SYSTEM\CurrentControlSet\Services\";
+        private const string REGISTRY_STARTUP_KEYNAME = "Start";
+        private ServiceStartupType _startupType;
+        #endregion
 
-        public static async Task<bool> ContinueAsync(string serviceName, TimeSpan timeout)
-            => await Task.Run(() => Continue(serviceName, timeout));
-        
-        public static bool Pause(string serviceName, TimeSpan timeout)
-            => HandleService(serviceName, service => { service.Pause(); }, ServiceControllerStatus.Paused, timeout);
 
-        public static async Task<bool> PauseAsync(string serviceName, TimeSpan timeout)
-            => await Task.Run(() => Pause(serviceName, timeout));
-
-        public static bool Restart(string serviceName, TimeSpan timeout)
-            => Stop(serviceName, timeout) && Start(serviceName, timeout);
-
-        public static async Task<bool> RestartAsync(string serviceName, TimeSpan timeout)
-            => await Task.Run(() => Restart(serviceName, timeout));
-
-        public static bool Start(string serviceName, TimeSpan timeout)
-            => HandleService(serviceName, service => { service.Start(); }, ServiceControllerStatus.Running, timeout);
-
-        public static async Task<bool> StartAsync(string serviceName, TimeSpan timeout)
-            => await Task.Run(() => Start(serviceName, timeout));
-
-        public static bool Stop(string serviceName, TimeSpan timeout)
-            => HandleService(serviceName, service => { service.Stop(); }, ServiceControllerStatus.Stopped, timeout);
-
-        public static async Task<bool> StopAsync(string serviceName, TimeSpan timeout)
-            => await Task.Run(() => Stop(serviceName, timeout));
-
-        public static ServiceControllerStatus GetStatus(string serviceName)
+        #region  Constructors & Destructor
+        public Service(string serviceName)
         {
-            using (var service = new ServiceController(serviceName))
+            Controller = new ServiceController(serviceName);
+            RegistryKey = Registry.LocalMachine.OpenSubKey(REGISTRY_SERVICE_PATH + serviceName, true);
+            StartupType = GetRegistryStartupType();
+        }
+        #endregion
+
+
+        #region  Properties & Indexers
+        public ServiceController Controller { get; set; }
+
+        public RegistryKey RegistryKey { get; set; }
+
+        public ServiceStartupType StartupType
+        {
+            get { return _startupType; }
+            set
             {
-                return service.Status;
+                if (_startupType == value) return;
+
+                _startupType = value;
+                SetRegistryStarupType(value);
             }
         }
         #endregion
 
 
+        #region Methods
+        public bool Continue(TimeSpan timeout) => Handle(() =>
+        {
+            Controller.Continue();
+            Controller.WaitForStatus(ServiceControllerStatus.Running, timeout);
+        });
+
+        public async Task<bool> ContinueAsync(TimeSpan timeout)
+            => await Task.Run(() => Continue(timeout));
+
+        public void Dispose()
+        {
+            Controller?.Dispose();
+            RegistryKey?.Dispose();
+        }
+
+        public bool Pause(TimeSpan timeout) => Handle(() =>
+        {
+            Controller.Pause();
+            Controller.WaitForStatus(ServiceControllerStatus.Paused, timeout);
+        });
+
+        public async Task<bool> PauseAsync(TimeSpan timeout)
+            => await Task.Run(() => Pause(timeout));
+
+        public bool Restart(TimeSpan timeout) => Stop(timeout) && Start(timeout);
+
+        public async Task<bool> RestartAsync(TimeSpan timeout)
+            => await Task.Run(() => Restart(timeout));
+
+        public bool Start(TimeSpan timeout) => Handle(() =>
+        {
+            Controller.Start();
+            Controller.WaitForStatus(ServiceControllerStatus.Running, timeout);
+        });
+
+        public async Task<bool> StartAsync(TimeSpan timeout)
+            => await Task.Run(() => Start(timeout));
+
+        public bool Stop(TimeSpan timeout) => Handle(() =>
+        {
+            Controller.Stop();
+            Controller.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+        });
+
+        public async Task<bool> StopAsync(TimeSpan timeout)
+            => await Task.Run(() => Stop(timeout));
+        #endregion
+
+
         #region Implementation
-        private static bool HandleService(string serviceName, Action<ServiceController> serviceHandler,
-            ServiceControllerStatus desiredStatus, TimeSpan timeout)
+        private ServiceStartupType GetRegistryStartupType()
+            => (ServiceStartupType)RegistryKey.GetValue(REGISTRY_STARTUP_KEYNAME);
+
+        private static bool Handle(Action handler)
         {
             try
             {
-                using (var service = new ServiceController(serviceName))
-                {
-                    serviceHandler(service);
-                    service.WaitForStatus(desiredStatus, timeout);
-                    return true;
-                }
+                handler();
+                return true;
             }
             catch
             {
                 return false;
             }
         }
+
+        private void SetRegistryStarupType(ServiceStartupType value)
+            => RegistryKey.SetValue(REGISTRY_STARTUP_KEYNAME, value);
         #endregion
     }
 }
